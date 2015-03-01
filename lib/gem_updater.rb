13f969203e57f2e7359d85702b704279618a6585
Maxime Demolin <maxime.demolin@laposte.net>
2015-03-01 17:12:25 +0100

    ADD support for templates
    Users may wish to have other template than the default one

--- lib/gem_updater.rb
+++ lib/gem_updater.rb
@@ -22,15 +22,27 @@ module GemUpdater
 
         @gemfile.changes[ gem_name ][ :changelog ] = source_page.changelog if source_page.changelog
       end
+    end
 
-      # Format the diff to get human readable information
-      # on the gems that were updated.
-      def format_diff
-        @gemfile.changes.each do |gem, details|
-          puts "* #{gem} #{details[ :versions ][ :old ]} → #{details[ :versions ][ :new ]}"
-          puts "[changelog](#{details[ :changelog ]})" if details[ :changelog ]
-          puts
-        end
+    # Format the diff to get human readable information
+    # on the gems that were updated.
+    def format_diff
+      @gemfile.changes.each do |gem, details|
+        puts ERB.new( template, nil, '<>' ).result( binding )
+      end
+    end
+
+    private
+
+    # Get the template for gem's diff.
+    # It can use a custom template.
+    #
+    # @return [ERB] the template
+    def template
+      @template ||= begin
+        File.read( "#{Dir.home}/.gem_updater_template.erb" )
+      rescue Errno::ENOENT
+        File.read( File.expand_path( '../../lib/gem_updater_template.erb', __FILE__ ) )
       end
     end
   end
