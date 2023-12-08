e4a02cd8e1167e4889946c55f7bb7956254b2deb
Maxime Demolin <maxime_dev@demol.in>
2023-12-08 18:09:38 +0100

    FIX acceptance specs (#177)
    !175 introduced a regression.

--- spec/acceptance/gem_update_spec.rb
+++ spec/acceptance/gem_update_spec.rb
@@ -5,43 +5,27 @@ require 'spec_helper'
 RSpec.describe GemUpdater do
   let(:updater) { GemUpdater::Updater.new }
 
-  around(:example) do |example|
-    current_working_directory = Dir.pwd
-
-    Dir.chdir('spec/acceptance/')
-    example.run
-    Dir.chdir(current_working_directory)
-
-    `git restore Gemfile.lock`
-  end
+  after { `git restore spec/acceptance/Gemfile.lock` }
 
   it 'outputs changelogs',
      vcr: { cassette_name: 'acceptance', record: :new_episodes } do
-    updater.update!([])
+    updater.update!(['--gemfile=spec/acceptance/Gemfile'])
     expect { updater.output_diff }.to output(<<~OUTPUT).to_stdout
+      * activesupport 7.0.0 → 7.1.2
+      [changelog](https://github.com/rails/rails/blob/v7.1.2/activesupport/CHANGELOG.md#rails-712-november-10-2023)
+
       * json 2.6.3 → 2.7.1
       [changelog](https://github.com/flori/json/blob/master/CHANGES.md#2023-12-05-271)
-
-      * mini_portile2 2.8.4 → 2.8.5
-
-      * nokogiri 1.15.4 → 1.15.5
-      [changelog](https://nokogiri.org/CHANGELOG.html)
 
       * parser 3.2.2.3 → 3.2.2.4
       [changelog](https://github.com/whitequark/parser/blob/v3.2.2.4/CHANGELOG.md)
 
-      * public_suffix 5.0.3 → 5.0.4
-      [changelog](https://github.com/weppos/publicsuffix-ruby/blob/master/CHANGELOG.md#504)
-
       * racc 1.7.1 → 1.7.3
 
-      * rake 13.0.6 → 13.1.0
-      [changelog](https://github.com/ruby/rake/blob/v13.1.0/History.rdoc)
-
-      * regexp_parser 2.8.1 → 2.8.3
+      * regexp_parser 2.6.1 → 2.8.3
       [changelog](https://github.com/ammar/regexp_parser/blob/master/CHANGELOG.md#283---2023-12-04---janosch-müller)
 
-      * rubocop 1.56.4 → 1.58.0
+      * rubocop 1.38.0 → 1.58.0
       [changelog](https://github.com/rubocop/rubocop/blob/master/CHANGELOG.md#1580-2023-12-01)
 
       * rubocop-ast 1.29.0 → 1.30.0
