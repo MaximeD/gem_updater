d022c12fd4d63a848f78d163724fb7fd84ecd667
Maxime Demolin <maxime_dev@demol.in>
2024-06-12 06:53:46 +0200

    UPDATE gems
    * addressable 2.8.5 → 2.8.6
    [changelog](https://github.com/sporkmonger/addressable/blob/main/CHANGELOG.md#v2.8.6)
    
    * crack 0.4.5 → 1.0.0
    [changelog](https://github.com/jnunemaker/crack/blob/master/History)
    
    * diff-lcs 1.5.0 → 1.5.1
    
    * hashdiff 1.0.1 → 1.1.0
    [changelog](https://github.com/liufengyun/hashdiff/blob/master/changelog.md#v110-2023-12-14)
    
    * json 2.7.1 → 2.7.2
    [changelog](https://github.com/flori/json/blob/master/CHANGES.md)
    
    * method_source 1.0.0 → 1.1.0
    [changelog](https://github.com/banister/method_source/blob/master/CHANGELOG.md#v110-april-15-2024)
    
    * mini_portile2 2.8.5 → 2.8.7
    
    * nokogiri 1.15.5 → 1.16.5
    [changelog](https://nokogiri.org/CHANGELOG.html)
    
    * parallel 1.23.0 → 1.25.1
    
    * parser 3.2.2.4 → 3.3.2.0
    [changelog](https://github.com/whitequark/parser/blob/v3.3.2.0/CHANGELOG.md)
    
    * public_suffix 5.0.4 → 5.0.5
    [changelog](https://github.com/weppos/publicsuffix-ruby/blob/master/CHANGELOG.md)
    
    * racc 1.7.3 → 1.8.0
    
    * rake 13.1.0 → 13.2.1
    [changelog](https://github.com/ruby/rake/blob/v13.2.1/History.rdoc)
    
    * regexp_parser 2.8.3 → 2.9.2
    [changelog](https://github.com/ammar/regexp_parser/blob/master/CHANGELOG.md#292---2024-05-15---janosch-müller)
    
    * rexml 3.2.6 → 3.3.0
    
    * rspec 3.12.0 → 3.13.0
    
    * rspec-core 3.12.2 → 3.13.0
    [changelog](https://github.com/rspec/rspec-core/blob/v3.13.0/Changelog.md)
    
    * rspec-expectations 3.12.3 → 3.13.0
    [changelog](https://github.com/rspec/rspec-expectations/blob/v3.13.0/Changelog.md#3130--2024-02-04)
    
    * rspec-mocks 3.12.6 → 3.13.1
    [changelog](https://github.com/rspec/rspec-mocks/blob/v3.13.1/Changelog.md)
    
    * rspec-support 3.12.1 → 3.13.1
    [changelog](https://github.com/rspec/rspec-support/blob/v3.13.1/Changelog.md)
    
    * rubocop 1.58.0 → 1.64.1
    [changelog](https://github.com/rubocop/rubocop/releases/tag/v1.64.1)
    
    * rubocop-ast 1.30.0 → 1.31.3
    [changelog](https://github.com/rubocop/rubocop-ast/blob/master/CHANGELOG.md#1313-2024-04-29)
    
    * rubocop-performance 1.19.1 → 1.21.0
    [changelog](https://github.com/rubocop/rubocop-performance/blob/master/CHANGELOG.md#1210-2024-03-30)
    
    * webmock 3.19.1 → 3.23.1
    [changelog](https://github.com/bblimke/webmock/blob/v3.23.1/CHANGELOG.md#3231)

--- spec/acceptance/gem_update_spec.rb
+++ spec/acceptance/gem_update_spec.rb
@@ -7,33 +7,50 @@ RSpec.describe GemUpdater do
 
   after { `git restore spec/acceptance/Gemfile.lock` }
 
-  it 'outputs changelogs',
-     vcr: { cassette_name: 'acceptance', record: :new_episodes } do
-    updater.update!(['--gemfile=spec/acceptance/Gemfile'])
-    expect { updater.output_diff }.to output(<<~OUTPUT).to_stdout
-      * activesupport 7.0.0 → 7.1.2
-      [changelog](https://github.com/rails/rails/blob/v7.1.2/activesupport/CHANGELOG.md#rails-712-november-10-2023)
+  let(:diff) do
+    <<~OUTPUT
+      * activesupport 7.0.0 → 7.1.3.4
+      [changelog](https://github.com/rails/rails/blob/v7.1.2/activesupport/CHANGELOG.md)
 
-      * json 2.6.3 → 2.7.1
-      [changelog](https://github.com/flori/json/blob/master/CHANGES.md#2023-12-05-271)
+      * concurrent-ruby 1.2.2 → 1.3.3
+      [changelog](https://github.com/ruby-concurrency/concurrent-ruby/blob/master/CHANGELOG.md#release-v133-9-june-2024)
 
-      * parser 3.2.2.3 → 3.2.2.4
+      * i18n 1.14.1 → 1.14.5
+      [changelog](https://github.com/ruby-i18n/i18n/releases)
+
+      * json 2.6.3 → 2.7.2
+      [changelog](https://github.com/flori/json/blob/master/CHANGES.md)
+
+      * minitest 5.20.0 → 5.23.1
+      [changelog](https://github.com/minitest/minitest/blob/master/History.rdoc)
+
+      * parallel 1.23.0 → 1.25.1
+
+      * parser 3.2.2.3 → 3.3.2.0
       [changelog](https://github.com/whitequark/parser/blob/v3.2.2.4/CHANGELOG.md)
 
-      * racc 1.7.1 → 1.7.3
+      * racc 1.7.1 → 1.8.0
 
-      * regexp_parser 2.6.1 → 2.8.3
-      [changelog](https://github.com/ammar/regexp_parser/blob/master/CHANGELOG.md#283---2023-12-04---janosch-müller)
+      * regexp_parser 2.6.1 → 2.9.2
+      [changelog](https://github.com/ammar/regexp_parser/blob/master/CHANGELOG.md)
 
-      * rubocop 1.38.0 → 1.58.0
-      [changelog](https://github.com/rubocop/rubocop/blob/master/CHANGELOG.md#1580-2023-12-01)
+      * rexml 3.2.6 → 3.3.0
 
-      * rubocop-ast 1.29.0 → 1.30.0
-      [changelog](https://github.com/rubocop/rubocop-ast/blob/master/CHANGELOG.md#1300-2023-10-26)
+      * rubocop 1.38.0 → 1.64.1
+      [changelog](https://github.com/rubocop/rubocop/blob/master/CHANGELOG.md)
+
+      * rubocop-ast 1.29.0 → 1.31.3
+      [changelog](https://github.com/rubocop/rubocop-ast/blob/master/CHANGELOG.md)
 
       * unicode-display_width 2.4.2 → 2.5.0
       [changelog](https://github.com/janlelis/unicode-display_width/blob/main/CHANGELOG.md#250)
 
     OUTPUT
+  end
+
+  it 'outputs changelogs',
+     vcr: { cassette_name: 'acceptance', record: :new_episodes } do
+    updater.update!(['--gemfile=spec/acceptance/Gemfile'])
+    expect { updater.output_diff }.to output(diff).to_stdout
   end
 end
